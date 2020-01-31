de1e5c8ea82ba00af6221f3db90db96e9005583c
Peter deHaan <peter@deseloper.com>
2020-01-30 18:34:08 -0800

    FIRST BLOOD

---
+++ .eleventy.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+const del = require("del");
+const Nunjucks = require("nunjucks");
+
+const dir = {
+  input: "src",
+  output: "_site",
+  includes: "_includes"
+};
+
+const includesDir = path.join(dir.input, dir.includes);
+
+del.sync(dir.output);
+
+module.exports = eleventyConfig => {
+  
+  // const env = new Nunjucks.Environment(
+  //   // we need to pass in our includes dir here
+  //   new Nunjucks.FileSystemLoader([includesDir]),
+  //   { autoescape: false }
+  // );
+  
+
+  const env = Nunjucks.configure(includesDir, {
+    autoescape: false
+  });
+
+  eleventyConfig.setLibrary("njk", env);
+  return {
+    dir
+  };
+};
